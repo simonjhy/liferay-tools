b5f90de7db643ada35b08a3c9451c9f8a2d91f9f
Gregory Amerson <gregory.amerson@liferay.com>
2014-08-19 16:28:38 +0800

    First test gradle-bundle-plugin

---
+++ test-modules/liferay-gradle-bundle-test/src/main/java/com/liferay/osgi/integration/tests/servicewrapper/B_UserLocalServiceWrapper.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.osgi.integration.tests.servicewrapper;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceWrapper;
+import com.liferay.portal.service.UserLocalService;
+import com.liferay.portal.service.UserLocalServiceWrapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(
+	immediate = true,
+	service = ServiceWrapper.class
+)
+public class B_UserLocalServiceWrapper extends UserLocalServiceWrapper {
+
+	public B_UserLocalServiceWrapper() {
+		super(null);
+	}
+
+	public B_UserLocalServiceWrapper(UserLocalService userLocalService) {
+		super(userLocalService);
+	}
+
+	@Override
+	public User getUserById(long userId) throws PortalException,
+		SystemException {
+
+		System.out.print("[B]");
+
+		User user = super.getUserById(userId);
+
+		System.out.println();
+
+		return user;
+	}
+
+}
