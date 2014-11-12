8c9eecfdb709a131ce7b98a6d94aee4a4f3d244a
Gregory Amerson <gregory.amerson@liferay.com>
2014-11-12 17:30:16 +0800

    Initial bndtools style project config and init com.liferay.tools bundle.

---
+++ com.liferay.tools/src/com/liferay/tools/sourceformatter/FTLSourceProcessor.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.tools.sourceformatter;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * @author Hugo Huijser
+ */
+public class FTLSourceProcessor extends BaseSourceProcessor {
+
+	@Override
+	protected String doFormat(
+			File file, String fileName, String absolutePath, String content)
+		throws Exception {
+
+		return trimContent(content, false);
+	}
+
+	@Override
+	protected void format() throws Exception {
+		String[] excludes = new String[] {
+			"**\\journal\\dependencies\\template.ftl",
+			"**\\servicebuilder\\dependencies\\props.ftl"
+		};
+		String[] includes = new String[] {"**\\*.ftl"};
+
+		List<String> fileNames = getFileNames(excludes, includes);
+
+		for (String fileName : fileNames) {
+			format(fileName);
+		}
+	}
+
+}
