122924b16eb2236c76b359010120aa5f7f36e566
Gregory Amerson <gregory.amerson@liferay.com>
2014-11-14 08:43:06 +0800

    Added basic compiler tests

--- com.liferay.tools.sass/test/com/liferay/tools/sass/tests/CompilerTests.java
+++ com.liferay.tools.sass/test/com/liferay/tools/sass/tests/CompilerTests.java
@@ -1,5 +1,6 @@
 package com.liferay.tools.sass.tests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -8,10 +9,16 @@ import com.liferay.tools.sass.SassCompiler;
 
 public class CompilerTests extends LibraryTests {
 
+	private static final String BASE_RESOURCES = "bin_test/com/liferay/tools/sass/tests/resources/";
+
 	@Test
-	public void testSharedLibraryLoading() throws Exception {
+	public void testSassCompiler() throws Exception {
 		SassCompiler compiler = new SassCompiler();
-		//String output = compiler.compileFile("", "", "");
 		assertNotNull(compiler);
+
+		String output = compiler.compileFile(BASE_RESOURCES + "_asset_category_selector.scss", "", "");
+		assertNotNull(output);
+		assertEquals(readFileContents(BASE_RESOURCES + "_asset_category_selector.css"), output);
 	}
+
 }
--- com.liferay.tools.sass/test/com/liferay/tools/sass/tests/LibraryTests.java
+++ com.liferay.tools.sass/test/com/liferay/tools/sass/tests/LibraryTests.java
@@ -1,6 +1,10 @@
 package com.liferay.tools.sass.tests;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.lang.reflect.Method;
 
 import org.junit.Before;
@@ -17,6 +21,38 @@ public class LibraryTests {
 		String prefixPath = (String)prefix.invoke(null);
 		String path = new File("resources/" + prefixPath).getCanonicalPath();
 		NativeLibrary.addSearchPath("sass", path);
+	}
+
+	protected String readFileContents(String filename) throws Exception {
+		return readStreamToString(new FileInputStream(new File(filename)));
+	}
+
+	protected String readStreamToString(InputStream contents) throws Exception {
+	    if( contents == null )
+	    {
+	        return null;
+	    }
+	
+	    final char[] buffer = new char[0x10000];
+	
+	    StringBuilder out = new StringBuilder();
+	
+	    Reader in = new InputStreamReader( contents, "UTF-8" ); //$NON-NLS-1$
+	
+	    int read;
+	    do
+	    {
+	        read = in.read( buffer, 0, buffer.length );
+	        if( read > 0 )
+	        {
+	            out.append( buffer, 0, read );
+	        }
+	    }
+	    while( read >= 0 );
+	
+	    contents.close();
+	
+	    return out.toString();
 	}
 
 }
